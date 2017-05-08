9305b749afba8b61bbd54d86cc0dc98f7fc7aa93
Walter Heredia <w.heredia@blcges.com>
2017-05-08 17:25:06 -0300

    p

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
         </div><!--/.nav-collapse -->
       </div>
     </nav>
-
+<p></p>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
