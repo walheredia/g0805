5d6db54921e338a54bdef1e0a2d3b47898b2ac27
Walter Heredia <w.heredia@blcges.com>
2017-05-09 15:15:24 -0300

    estilos

--- index.php
+++ index.php
@@ -13,8 +13,10 @@
 
   </head>
   <body>
+  <div class="nav-row" style="height: 15px; width: 100%;">
+  </div>
     <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top bg-color">
+    <nav class="navbar navbar-inverse bg-color">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -40,41 +42,32 @@
         </div><!--/.nav-collapse -->
       </div>
     </nav>
-
-      <div id="carousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
+    <!--End navbar -->
+    <!--start carousel -->
+    <div id="contenedor" style="width: 100%; padding-top: 52px;" class="center-block">
+      <div id="myCarousel" class="carousel slide">
         <ol class="carousel-indicators">
-          <li data-target="#carousel" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+          <li data-target="#myCarousel" data-slide-to="4"></li>
         </ol>
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img src="images/1.jpg" alt="..." class="center-block">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
-          <div class="item">
-            <img src="images/2.jpg" alt="..." class="center-block">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
+        <!-- Carousel items -->
+        <div class="carousel-inner">
+          <div class="active item"><img  src="images/1.jpg" alt="banner1" class="img-responsive center-block" /></div>
+          <div class="item"><img  src="images/2.jpg" alt="banner2" class="img-responsive center-block" /></div>
+          <div class="item"><img  src="images/1.jpg" alt="banner3" class="img-responsive center-block" /></div>
+          <div class="item"><img  src="images/2.jpg" alt="banner4" class="img-responsive center-block" /></div>
+          <div class="item"><img  src="images/1.jpg" alt="banner5" class="img-responsive center-block" /></div>
         </div>
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+        <!-- Carousel nav 
+        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+        -->
       </div>
+  </div>
 
-    
-  </body>
   <script src="js/jquery-3.2.1.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script type="text/javascript">
@@ -84,5 +77,8 @@
           interval: 3000
         })
       });
-    </script>
+  </script>
+    
+  </body>
+  
 </html>
